977a9dbcb172b3c4b752c6764d43bd1fefa77c5b
Shad Azam <azamshad08@gmail.com>
2024-02-22 16:48:31 +0530

    update

--- server/server.js
+++ server/server.js
@@ -26,7 +26,8 @@ const corsOptions = {
     optionsSuccessStatus: 200,
 }
 // MIDDLEWARE
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 // app.use(express.static(path.join(__dirname, 'client')));
